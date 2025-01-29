448cce4d7d1c5bee4ca2dc219734f6235044fc08
allisonjuliana <162182668+allisonjuliana@users.noreply.github.com>
2025-01-29 13:47:48 +0000

    todo list llamando API

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,117 +2,104 @@ import React, { useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Home = () => {
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("");
   const [all, setAll] = useState([]);
-  const [idDelete, setIdDelete] = useState([]);
+console.log(all);
 
-  useEffect(()=>{
-    if(idDelete > 0 ){
-    function cutTask(){
-        const requestOptions = {
+  function deleteUser(){
+    const requestOptions = {
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify()
+    };
+    fetch('https://playground.4geeks.com/todo/users/milton_diaz', requestOptions)
+    .then((data) => data)
+  };
+  function postUser(){
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify()
+    };
+    fetch('https://playground.4geeks.com/todo/users/milton_diaz', requestOptions)
+    .then((data) => data)
+  };
+  function addTask(){
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          "label": inputValue,
+          "is_done": false
+        })
+      };
+      fetch('https://playground.4geeks.com/todo/todos/milton_diaz', requestOptions)
+      .then(response => response.json())
+      .then((data) => setAll([...all,data]))
+    };
+    function cutTask(idAsk){
+      const requestOptions = {
           method: 'DELETE',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify()
         };
-        fetch("https://playground.4geeks.com/todo/todos/"+idDelete, requestOptions)
-        .then((data) => data)
-      };
-      cutTask();
-      }else{}
-  },[idDelete]);
-
+        fetch("https://playground.4geeks.com/todo/todos/"+idAsk, requestOptions)
+      .then((data) => data)
+    };
+    function getTask(){                      
+      fetch('https://playground.4geeks.com/todo/users/milton_diaz')
+      .then((response) => response.json())
+      .then((data) => setAll(data.todos))
+    };
+    useEffect(()=>{
+      getTask();
+    },[]);
   return (
     <div className="container">
       <div className="titulo">
         <p>todos</p>
       </div>
-      <div className="list">
+      <div className="list">        
         <div className="header">
           <input
             type="text"
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
             onKeyDown={(e) => {
-              if(e.key === "Enter"){
-
-                function addTask(){
-                  const requestOptions = {
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({
-                      "label": inputValue,
-                      "is_done": false
-                    })
-                  };
-                  fetch('https://playground.4geeks.com/todo/todos/milton_diaz', requestOptions)
-                  .then(response => response.json())
-                  .then((data) => data)
-                };
+              if(e.key === "Enter"){                
                 addTask();
-
-                setAll(all.concat([inputValue]));
+                {all.map((item, index) => <p key={index}>{item.label}</p>)}
                 setInputValue("");
               }
             }}
             placeholder="What needs to be done?">
           </input>
-        </div>
+        </div>        
         {all.map((item, index) => (
-            <div className="items">{item}
-              <button key={index} className="boton"
+            <div className="items" key={index}>{item.label}
+              <button  className="boton"
                 onClick={() => {
-
-                  setAll(all.filter((t, currentIndex) => index != currentIndex));
-
-                  function getTask(){                      
-                    fetch('https://playground.4geeks.com/todo/users/milton_diaz')
-                    .then((response) => response.json())
-                    .then((data) => setIdDelete(data.todos[index].id))
-                  };
-                  getTask();
-
+                  cutTask(item.id);
+                  window.location.reload();
                 }}><AiOutlineClose />
               </button>
             </div>
           ))
-        }
+        }        
         <div className="footer">
           <div>
             {all.length} items left
           </div>
-
           <div>
             <button className="deleteAll"
-
               onClick={() => {
-
-                setTimeout(() => {
-                  function postUser(){
-                    const requestOptions = {
-                      method: 'POST',
-                      headers: {'Content-Type': 'application/json'},
-                      body: JSON.stringify()
-                    };
-                    fetch('https://playground.4geeks.com/todo/users/milton_diaz', requestOptions)
-                    .then((data) => data)
-                  };
-                  postUser();
-                }, 1000);
-
-                function deleteUser(){
-                  const requestOptions = {
-                    method: 'DELETE',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify()
-                  };
-                  fetch('https://playground.4geeks.com/todo/users/milton_diaz', requestOptions)
-                  .then((data) => data)
-                };
                 deleteUser();
-
-                setAll([]);
-                
-
+                setTimeout(() => {
+                  postUser();
+                }, 100);
+                setTimeout(() => {
+                  window.location.reload();
+                }, 200);
               }}>Eliminar Tareas
             </button>
           </div>
@@ -123,6 +110,5 @@ const Home = () => {
       <div className="hoja3"></div>
     </div>
   );
-};
- 
+}; 
 export default Home;
